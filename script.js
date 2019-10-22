99010a3f406609b596d6cd3bda1df1655a09a20d
Marina-bott <marinabott.pro@gmail.com>
2019-10-22 13:36:25 +0200

    prout

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let bouton = document.getElementById("bouton");
+let bouton2 = document.getElementById("bouton2");
+let accepter = document.getElementById("accepter");
 
 bouton.onclick = function()
 {
     $( "#dialog" ).dialog( "close" );
+    accepter.innerText = "Vous avez accepter";
+
+}
+bouton2.onclick = function()
+{
+  $( "#dialog" ).dialog( "close" );
+  accepter.innerText = "Vous avez refuser";
 }
